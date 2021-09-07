791ee276fa4b76d113f09893d894ef135635f200
imjord <dean.jay.goat@gmail.com>
2021-09-07 15:13:26 -0600

    able to save an object store when app is offline

---
+++ public/js/idb.js
@@ -0,0 +1,53 @@
+
+
+
+// variable to hold db connection 
+let db; 
+
+// establish a connection to indexDB database called budget and set version to 1
+
+
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function(event){
+    // save a ref to the data base
+    const db = event.target.result;
+    // create an object store called new_amount set it to have an auto increment 
+    db.createObjectStore('new_amount', { autoIncrement: true });
+}
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        saveAmount();
+    }
+}
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+} 
+
+
+// function to execute with no internet 
+function saveAmount(record) {
+    const transaction = db.transaction(['new_amount'], 'readwrite');
+
+    const amountObjectStore = transaction.objectStore('new_amount');
+
+    amountObjectStore.add(record);
+}
+
+
+function uploadAmount() {
+    const transaction = db.transaction(['new_amount'], 'readwrite');
+
+    // access my store object
+    const amountObjectStore = transaction.objectStore('new_amount');
+
+    // get all the records 
+
+    const getAll = amountObjectStore.getAll();
+
+    // fa
+}
--- public/js/index.js
+++ public/js/index.js
@@ -136,7 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
-    saveRecord(transaction);
+    saveAmount(transaction);
 
     // clear form
     nameEl.value = "";
