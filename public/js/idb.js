116528c388d8787b06119a3e0c58db8077a39e2b
imjord <dean.jay.goat@gmail.com>
2021-09-07 15:42:56 -0600

    able to upload offline data objects

--- public/js/idb.js
+++ public/js/idb.js
@@ -50,4 +50,35 @@ function uploadAmount() {
     const getAll = amountObjectStore.getAll();
 
     // fa
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST', 
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_amount'], 'readwrite');
+                const amountObjectStore = transaction.objectStore('new_amount');
+                amountObjectStore.clear();
+
+                alert('all saved transactions have been submitted')
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }
 }
+
+
+
+window.addEventListener('online', uploadAmount);
